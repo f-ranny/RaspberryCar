c85730ca7c5d917ba4caecf2e00a26e8f684ec17
f-ranny <69907754+f-ranny@users.noreply.github.com>
2021-10-19 15:59:48 +0300

    Add stream feature

---
+++ test/TestRaspberryCar/riding/hardware/camera.py
@@ -0,0 +1,23 @@
+import io
+from picamera import PiCamera
+from threading import Condition
+
+
+class StreamingOutput(object):
+    def __init__(self):
+        self.frame = None
+        self.buffer = io.BytesIO()
+        self.condition = Condition()
+
+    def write(self, buf):
+        if buf.startswith(b'\xff\xd8'):
+            self.buffer.truncate()
+            with self.condition:
+                self.frame = self.buffer.getvalue()
+                self.condition.notify_all()
+            self.buffer.seek(0)
+        return self.buffer.write(buf)
+
+
+camera = PiCamera(resolution='640x480', framerate=24)
+output = StreamingOutput()
